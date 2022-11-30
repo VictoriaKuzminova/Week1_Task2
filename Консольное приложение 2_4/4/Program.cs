babcb15507709ad9fbc19933d7e13888f3657a07
VictoriaKuzminova <119403787+VictoriaKuzminova@users.noreply.github.com>
2022-11-30 19:00:58 +0300

    Add files via upload

---
+++ Консольное приложение 2_4/4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            for (int i = 1; i <= 5; ++i, Console.WriteLine())
+            {
+                for(int j = 1; j <= a; j++)
+                {
+                    Console.Write(" "+i);
+                }
+                a++;
+            }
+        }
+    }
+}
