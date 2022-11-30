2d5d576bd6f435f98e82b7f9310ab3663f6e4d43
VictoriaKuzminova <119403787+VictoriaKuzminova@users.noreply.github.com>
2022-11-30 18:18:52 +0300

    Add files via upload

---
+++ Консольное приложение 2_1/1/Program.cs
@@ -0,0 +1,50 @@
+﻿    using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float x, y;
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Введите координату x: ");
+                    x = float.Parse(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Некорректный ввод!");
+                }
+
+            }
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Введите координату y: ");
+                    y = float.Parse(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Некорректный ввод!");
+                }
+
+            }
+
+            if (y >= Math.Abs(x) && x * x + y * y <=225 )
+            {
+                if (y>Math.Abs(x) && x * x + y * y < 225)
+                    Console.WriteLine("Да");
+                else
+                    Console.WriteLine("На границе");
+            }
+            else
+                Console.WriteLine("Нет");
+        }
+    }
+}
